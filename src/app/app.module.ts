2154d5a8e872e61da79733ccfe40b4fc469f0d9c
Vincent Baylly <vincent.baylly@gmail.com>
2020-12-12 10:09:27 -0500

    import the directive in the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './components/FirstComponent/firstcomponent.component';
 import { MyEventComponent } from './components/MyEvent/myevent.component';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponent, MyEventComponent],
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    MyEventComponent,
+    HighlightDirective,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
