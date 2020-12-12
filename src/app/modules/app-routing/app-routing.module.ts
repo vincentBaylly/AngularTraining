b68939592949a777bda5b19defad895bd9562906
Vincent Baylly <vincent.baylly@gmail.com>
2020-12-12 15:05:19 -0500

    route A and B components

--- src/app/modules/app-routing/app-routing.module.ts
+++ src/app/modules/app-routing/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AComponent } from '../../components/a/a.component';
+import { BComponent } from '../../components/b/b.component';
 
 // un tableau contenant nos routes!
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'A',
+    component: AComponent,
+  },
+  {
+    path: 'B',
+    component: BComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
